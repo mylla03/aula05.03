eb08e17393084a904cc650329ed96e63ac44e93c
mylla03 <144168339+mylla03@users.noreply.github.com>
2024-03-06 15:34:30 -0300

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,13 @@
+n = 10
+
+n += 5
+print (n)
+
+n -= 3
+print(n)
+
+n *= 2
+print(n)
+
+n /= 4
+print(n)
---
+++ 02.py
@@ -0,0 +1,18 @@
+n = 10
+
+if n % 2 == 0:
+    print ("par")
+
+else:
+    print("impar")
+
+hora = 17
+
+if hora < 12:
+    print("bom dia!")
+
+elif hora > 12 and hora < 18:
+    print("boa tarde!")
+
+else:
+    print("boa noite!")
